9850fd56f86058fff7d361477868ec787eb42d9d
Eduardo Silva <eduardo.silva@spirt.pt>
2024-04-19 15:25:44 +0100

    SpringBoot Initializr base

---
+++ src/test/java/pt/psoft/g1/psoftg1/PsoftG1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.psoft.g1.psoftg1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsoftG1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
