348ac24f7ebbc47f10a234e32ae4b4e36eac720b
Eduardo Silva <eduardo.silva@spirt.pt>
2024-05-06 17:32:12 +0100

    Modified LendingController to find the reader based on the token used for the request

--- src/main/java/pt/psoft/g1/psoftg1/lendingmanagement/api/LendingController.java
+++ src/main/java/pt/psoft/g1/psoftg1/lendingmanagement/api/LendingController.java
@@ -7,14 +7,26 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pt.psoft.g1.psoftg1.exceptions.NotFoundException;
 import pt.psoft.g1.psoftg1.lendingmanagement.model.Lending;
 import pt.psoft.g1.psoftg1.lendingmanagement.services.CreateLendingDto;
 import pt.psoft.g1.psoftg1.lendingmanagement.services.LendingService;
+import pt.psoft.g1.psoftg1.readermanagement.model.ReaderDetails;
+import pt.psoft.g1.psoftg1.readermanagement.services.ReaderService;
+import pt.psoft.g1.psoftg1.usermanagement.model.User;
+import pt.psoft.g1.psoftg1.usermanagement.services.UserService;
+
+import java.nio.file.AccessDeniedException;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Optional;
 
 @Tag(name = "Lendings", description = "Endpoints for managing Lendings")
 @RestController
@@ -22,11 +34,15 @@ import pt.psoft.g1.psoftg1.lendingmanagement.services.LendingService;
 @RequestMapping("/api/lending")
 public class LendingController {
 
+    private final AuthenticationManager authenticationManager;
+
     private static final String IF_MATCH = "If-Match";
 
     private static final Logger logger = LoggerFactory.getLogger(LendingController.class);
 
     private final LendingService service;
+    private final ReaderService readerService;
+    private final UserService userService;
 
     private final LendingViewMapper lendingViewMapper;
 
@@ -44,16 +60,28 @@ public class LendingController {
         return ResponseEntity.created(newlendingUri).eTag(Long.toString(lending.getVersion()))
                 .body(lendingViewMapper.toLendingView(lending));
     }
-    
+
     @Operation(summary = "Gets a specific Lending")
     @GetMapping(value = "/{year}/{sequential}")
     public ResponseEntity<LendingView> findByLendingNumber(
+            Authentication authentication,
             @PathVariable("year")
                 @Parameter(description = "The year of the Lending to find")
                 final Integer year,
             @PathVariable("sequential")
                 @Parameter(description = "The sequencial of the Lending to find")
-                final Integer sequential) {
+                final Integer sequential) throws AccessDeniedException {
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String username = userDetails.getUsername();
+
+        Optional<User> optUser = this.userService.findByUsername(username);
+        if(optUser.isEmpty()) {
+            throw new AccessDeniedException("User is not logged in");
+        }
+
+        ReaderDetails readerDetails = null;
+        //TODO: Get readerDetails from the username
 
         String ln = year + "/" + sequential;
 
@@ -64,5 +92,5 @@ public class LendingController {
                 .eTag(Long.toString(lending.getVersion()))
                 .body(lendingViewMapper.toLendingView(lending));
     }
-    
+
 }
