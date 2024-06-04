ddb630b8ad75e8723a247515251178c6c312c30b
rmfranca <1221708@isep.ipp.pt>
2024-06-04 03:22:56 +0100

    More and better Postman requests/tests

--- src/main/java/pt/psoft/g1/psoftg1/exceptions/LendingForbiddenException.java
+++ src/main/java/pt/psoft/g1/psoftg1/exceptions/LendingForbiddenException.java
@@ -1,10 +1,11 @@
 package pt.psoft.g1.psoftg1.exceptions;
 
+import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.FORBIDDEN)
-public class LendingForbiddenException extends RuntimeException {
+public class LendingForbiddenException extends ValidationException {
 
     private static final long serialVersionUID = 1L;
 
