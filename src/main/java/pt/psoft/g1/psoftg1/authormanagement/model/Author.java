b25429895b63c7891ed08850eae98bce22cacd80
alvarenga <1220966@ISEP.IPP.P>
2024-06-05 00:24:28 +0100

    class author atualizada

--- src/main/java/pt/psoft/g1/psoftg1/authormanagement/model/Author.java
+++ src/main/java/pt/psoft/g1/psoftg1/authormanagement/model/Author.java
@@ -1,9 +1,16 @@
 package pt.psoft.g1.psoftg1.authormanagement.model;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.hibernate.StaleObjectStateException;
+import org.springframework.beans.factory.annotation.Value;
 import pt.psoft.g1.psoftg1.shared.model.Name;
+import pt.psoft.g1.psoftg1.shared.model.Photo;
+
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Entity
 public class Author {
@@ -20,10 +27,15 @@ public class Author {
     @Embedded
     private Name name;
 
-/*
-    @OneToOne(fetch = FetchType.LAZY)
+    @Transient
+    @Value("${file.upload-dir}")
+    private Path uploadDir;
+
+    @Nullable
+    @Getter
+    @OneToOne
+    @JoinColumn(name="photo_id", nullable = true)
     private Photo photo;
-*/
 
     @Embedded
     private Bio bio;
@@ -31,18 +43,43 @@ public class Author {
     public void setName(String name) {
         this.name = new Name(name);
     }
-
     public void setBio(String bio) {
         this.bio = new Bio(bio);
+    }
+
+    private void setPhotoInternal(String photo) {
+        if(photo == null) {
+            this.photo = null;
+            return;
+        }
+
+        this.photo = new Photo(Paths.get(photo));
     }
 
     public Long getVersion() { return version;}
 
     public Long getId() { return authorNumber;}
 
-    public Author(String name, String bio) {
+    public Author(String name, String bio, String photoURI) {
         setName(name);
         setBio(bio);
+        if(photoURI != null) {
+            try {
+                //If the Path object instantiation succeeds, it means that we have a valid Path
+                this.photo = new Photo(Paths.get(photoURI));
+            } catch (InvalidPathException e) {
+                //For some reason it failed, let's set to null to avoid invalid references to photos
+                this.photo = null;
+            }
+        } else {
+            this.photo = null;
+        }
+
+    }
+
+    @Nullable
+    public Photo getPhoto() {
+        return photo;
     }
 
     protected Author() {
@@ -50,7 +87,7 @@ public class Author {
     }
 
 
-    public void applyPatch(final long desiredVersion, final String name, final String bio) {
+    public void applyPatch(final long desiredVersion, final String name, final String bio, String photoURI) {
 
         if (this.version != desiredVersion) {
             throw new StaleObjectStateException("Object was already modified by another user", this.authorNumber);
@@ -62,7 +99,15 @@ public class Author {
         if (bio != null) {
             setBio(bio);
         }
+        if(photoURI != null) {
+            try {
+                setPhotoInternal(photoURI);
+            } catch(InvalidPathException ignored) {}
+        } else {
+            setPhotoInternal(null);
+        }
     }
+
 
     public String getName(){
         return this.name.toString();
