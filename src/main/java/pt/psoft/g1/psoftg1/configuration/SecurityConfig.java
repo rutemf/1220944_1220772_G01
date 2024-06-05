611f80b88843ea76457f8cc190be9827d8e3a12b
alvarenga <1220966@ISEP.IPP.P>
2024-06-05 23:18:55 +0100

    SecurityConfig permissoes mudadas

--- src/main/java/pt/psoft/g1/psoftg1/configuration/SecurityConfig.java
+++ src/main/java/pt/psoft/g1/psoftg1/configuration/SecurityConfig.java
@@ -124,10 +124,10 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET,"/api/authors/{authorNumber}").hasAnyRole(Role.READER, Role.LIBRARIAN)
                 .requestMatchers(HttpMethod.GET,"/api/authors").hasAnyRole(Role.READER, Role.LIBRARIAN)
                 .requestMatchers(HttpMethod.GET,"/api/authors/{authorNumber}/books").hasRole(Role.READER)
+                .requestMatchers(HttpMethod.GET,"/api/authors/photo").hasAnyRole(Role.READER, Role.LIBRARIAN)
+                .requestMatchers(HttpMethod.GET,"/api/authors/{authorNumber}/photo").hasAnyRole(Role.READER, Role.LIBRARIAN)
                 .requestMatchers(HttpMethod.GET,"/api/authors/top5").hasRole(Role.READER)
-                .requestMatchers(HttpMethod.GET,"/api/authors/photo").hasRole(Role.LIBRARIAN)
-                .requestMatchers(HttpMethod.GET,"/api/authors/{authornumber}/photo").hasRole(Role.LIBRARIAN)
-                //end authors
+                //end author
                 //books
                 .requestMatchers(HttpMethod.PUT,"/api/books/{isbn}").hasRole(Role.LIBRARIAN)
                 .requestMatchers(HttpMethod.PATCH,"/api/books/{isbn}").hasRole(Role.LIBRARIAN)
