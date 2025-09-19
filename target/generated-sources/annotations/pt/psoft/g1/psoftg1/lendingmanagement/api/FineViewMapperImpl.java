b1231053061e98067be20a887cb321a324de9e16
Miguel <1220772@isep.ipp.pt>
2025-09-19 22:31:54 +0100

    Datasource Config

--- target/generated-sources/annotations/pt/psoft/g1/psoftg1/lendingmanagement/api/FineViewMapperImpl.java
+++ target/generated-sources/annotations/pt/psoft/g1/psoftg1/lendingmanagement/api/FineViewMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-17T22:03:47+0100",
+    date = "2025-09-19T22:16:34+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
--- target/generated-sources/annotations/pt/psoft/g1/psoftg1/readermanagement/api/ReaderViewMapperImpl.java
+++ target/generated-sources/annotations/pt/psoft/g1/psoftg1/readermanagement/api/ReaderViewMapperImpl.java
@@ -13,7 +13,7 @@ import pt.psoft.g1.psoftg1.usermanagement.model.Reader;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-17T22:03:47+0100",
+    date = "2025-09-19T22:16:34+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
