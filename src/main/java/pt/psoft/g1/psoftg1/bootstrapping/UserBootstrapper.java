fde38b4e4343ce5a130086db7b5b68735e3cde00
Miguel <1220772@isep.ipp.pt>
2025-09-26 21:48:32 +0100

    Cleaning UserBootstrapper

--- src/main/java/pt/psoft/g1/psoftg1/bootstrapping/UserBootstrapper.java
+++ src/main/java/pt/psoft/g1/psoftg1/bootstrapping/UserBootstrapper.java
@@ -2,27 +2,13 @@ package pt.psoft.g1.psoftg1.bootstrapping;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import pt.psoft.g1.psoftg1.genremanagement.model.Genre;
-import pt.psoft.g1.psoftg1.genremanagement.repositories.GenreRepository;
-import pt.psoft.g1.psoftg1.readermanagement.model.ReaderDetails;
-import pt.psoft.g1.psoftg1.readermanagement.repositories.ReaderRepository;
 import pt.psoft.g1.psoftg1.usermanagement.model.Librarian;
 import pt.psoft.g1.psoftg1.usermanagement.model.Reader;
 import pt.psoft.g1.psoftg1.usermanagement.model.User;
 import pt.psoft.g1.psoftg1.usermanagement.repositories.UserRepository;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -30,17 +16,12 @@ import java.util.Optional;
 public class UserBootstrapper implements CommandLineRunner {
 
     private final UserRepository userRepository;
-    private final ReaderRepository readerRepository;
-    private final GenreRepository genreRepository;
-    private final JdbcTemplate jdbcTemplate;
-    private List<String> queriesToExecute = new ArrayList<>();
 
     @Override
     @Transactional
     public void run(final String... args)  {
         createReaders();
         createLibrarian();
-        executeQueries();
     }
 
     private void createReaders() {
@@ -73,13 +54,11 @@ public class UserBootstrapper implements CommandLineRunner {
     }
 
     private void createLibrarian(){
-        // Maria
+
         if (userRepository.findByUsername("maria@gmail.com").isEmpty()) {
-            final User maria = Librarian.newLibrarian("maria@gmail.com", "Mariaroberta!123", "Maria Roberta");
+            final User maria = Librarian.newLibrarian("maria@gmail.com", "Maria!123", "Maria Roberta");
             userRepository.save(maria);
         }
-    }
 
-    private void executeQueries() {
     }
 }
