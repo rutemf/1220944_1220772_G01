f5b3a51dc2e0ce79684951b730e082f64258ceb5
Miguel <1220772@isep.ipp.pt>
2025-10-06 18:48:21 +0100

    Pipeline Test

--- src/main/java/pt/psoft/g1/psoftg1/usermanagement/services/UserService.java
+++ src/main/java/pt/psoft/g1/psoftg1/usermanagement/services/UserService.java
@@ -44,38 +44,40 @@ import java.util.Optional;
 
 /**
  * Based on https://github.com/Yoh0xFF/java-spring-security-example
- *
  */
 @Service
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
 
-	private final UserRepository userRepo;
-	private final EditUserMapper userEditMapper;
+    private final UserRepository userRepo;
+    private final EditUserMapper userEditMapper;
 
-	private final ForbiddenNameRepository forbiddenNameRepository;
+    private final ForbiddenNameRepository forbiddenNameRepository;
 
-	private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
-	public List<User> findByName(String name){
-		return this.userRepo.findByNameName(name);
-	}
-	public List<User> findByNameLike(String name) { return this.userRepo.findByNameNameContains(name); }
+    public List<User> findByName(String name) {
+        return this.userRepo.findByNameName(name);
+    }
 
-	@Transactional
-	public User create(final CreateUserRequest request) {
-		if (userRepo.findByUsername(request.getUsername()).isPresent()) {
-			throw new ConflictException("Username already exists!");
-		}
+    public List<User> findByNameLike(String name) {
+        return this.userRepo.findByNameNameContains(name);
+    }
 
-		Iterable<String> words = List.of(request.getName().split("\\s+"));
-		for (String word : words){
-			if(!forbiddenNameRepository.findByForbiddenNameIsContained(word).isEmpty()) {
-				throw new IllegalArgumentException("Name contains a forbidden word");
-			}
-		}
+    @Transactional
+    public User create(final CreateUserRequest request) {
+        if (userRepo.findByUsername(request.getUsername()).isPresent()) {
+            throw new ConflictException("Username already exists!");
+        }
 
-		User user;
+        Iterable<String> words = List.of(request.getName().split("\\s+"));
+        for (String word : words) {
+            if (!forbiddenNameRepository.findByForbiddenNameIsContained(word).isEmpty()) {
+                throw new IllegalArgumentException("Name contains a forbidden word");
+            }
+        }
+
+        User user;
 
 		/*switch(request.getRole()) {
 			case Role.READER: {
@@ -91,71 +93,73 @@ public class UserService implements UserDetailsService {
 			}
 		}*/
 
-		user = Librarian.newLibrarian(request.getUsername(), request.getPassword(), request.getName());
-		//final User user = userEditMapper.create(request);
-		user.setPassword(passwordEncoder.encode(request.getPassword()));
-		//user.addAuthority(new Role(request.getRole()));
+        user = Librarian.newLibrarian(request.getUsername(), request.getPassword(), request.getName());
+        //final User user = userEditMapper.create(request);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        //user.addAuthority(new Role(request.getRole()));
 
-		return userRepo.save(user);
-	}
+        return userRepo.save(user);
+    }
 
-	@Transactional
-	public User update(final Long id, final EditUserRequest request) {
-		final User user = userRepo.getById(id);
-		userEditMapper.update(request, user);
+    @Transactional
+    public User update(final Long id, final EditUserRequest request) {
+        final User user = userRepo.getById(id);
+        userEditMapper.update(request, user);
 
-		return userRepo.save(user);
-	}
+        return userRepo.save(user);
+    }
 
-	@Transactional
-	public User delete(final Long id) {
-		final User user = userRepo.getById(id);
+    @Transactional
+    public User delete(final Long id) {
+        final User user = userRepo.getById(id);
 
-		// user.setUsername(user.getUsername().replace("@", String.format("_%s@",
-		// user.getId().toString())));
-		user.setEnabled(false);
-		return userRepo.save(user);
-	}
+        // user.setUsername(user.getUsername().replace("@", String.format("_%s@",
+        // user.getId().toString())));
+        user.setEnabled(false);
+        return userRepo.save(user);
+    }
 
-	@Override
-	public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
-		return userRepo.findByUsername(username).orElseThrow(
-				() -> new UsernameNotFoundException(String.format("User with username - %s, not found", username)));
-	}
+    @Override
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        return userRepo.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException(String.format("User with username - %s, not found", username)));
+    }
 
-	public boolean usernameExists(final String username) {
-		return userRepo.findByUsername(username).isPresent();
-	}
+    public boolean usernameExists(final String username) {
+        return userRepo.findByUsername(username).isPresent();
+    }
 
-	public User getUser(final Long id) {
-		return userRepo.getById(id);
-	}
+    public User getUser(final Long id) {
+        return userRepo.getById(id);
+    }
 
-	public Optional<User> findByUsername(final String username) { return userRepo.findByUsername(username); }
+    public Optional<User> findByUsername(final String username) {
+        return userRepo.findByUsername(username);
+    }
 
-	public List<User> searchUsers(Page page, SearchUsersQuery query) {
-		if (page == null) {
-			page = new Page(1, 10);
-		}
-		if (query == null) {
-			query = new SearchUsersQuery("", "");
-		}
-		return userRepo.searchUsers(page, query);
-	}
+    public List<User> searchUsers(Page page, SearchUsersQuery query) {
+        if (page == null) {
+            page = new Page(1, 10);
+        }
+        if (query == null) {
+            query = new SearchUsersQuery("", "");
+        }
+        return userRepo.searchUsers(page, query);
+    }
 
-	public User getAuthenticatedUser(Authentication authentication) {
-		if (authentication == null || !(authentication.getPrincipal() instanceof Jwt jwt)) {
-			throw new AccessDeniedException("User is not logged in");
-		}
+    public User getAuthenticatedUser(Authentication authentication) {
+        if (authentication == null || !(authentication.getPrincipal() instanceof Jwt jwt)) {
+            throw new AccessDeniedException("User is not logged in");
+        }
 
-		// split is present because jwt is storing the id before the username, separated by a comma
+        // split is present because jwt is storing the id before the username, separated by a comma
         String loggedUsername = jwt.getClaimAsString("sub").split(",")[1];
 
-		Optional<User> loggedUser = findByUsername(loggedUsername);
-		if (loggedUser.isEmpty()) {
-			throw new AccessDeniedException("User is not logged in");
-		}
+        Optional<User> loggedUser = findByUsername(loggedUsername);
+        if (loggedUser.isEmpty()) {
+            throw new AccessDeniedException("User is not logged in");
+        }
 
-		return loggedUser.get();
-	}
+        return loggedUser.get();
+    }
 }
