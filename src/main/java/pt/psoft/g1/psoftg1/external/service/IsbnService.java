1b4b76ef9433f2ab899514d7ef01980b1a5000a6
Miguel <1220772@isep.ipp.pt>
2025-12-28 18:48:41 +0000

    Deleting Old App

--- src/main/java/pt/psoft/g1/psoftg1/external/service/IsbnService.java
+++
@@ -1,5 +0,0 @@
-package pt.psoft.g1.psoftg1.external.service;
-
-public interface IsbnService {
-    String fetchIsbnsByTitle(String title);
-}
--- src/main/java/pt/psoft/g1/psoftg1/shared/repositories/PhotoRepository.java
+++
@@ -1,6 +0,0 @@
-package pt.psoft.g1.psoftg1.shared.repositories;
-
-public interface PhotoRepository {
-
-    void deleteByPhotoFile(String photoFile);
-}
