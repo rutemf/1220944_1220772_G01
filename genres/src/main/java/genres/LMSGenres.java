7cbaab5ade630f91c3f427ef56c391664f62be06
Miguel <1220772@isep.ipp.pt>
2026-01-03 13:14:52 +0000

    Genres Fix

--- genres/src/main/java/genres/LMSGenres.java
+++ genres/src/main/java/genres/LMSGenres.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LSMGenres {
+public class LMSGenres {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(LSMGenres.class, args);
+        SpringApplication.run(LMSGenres.class, args);
     }
 }
